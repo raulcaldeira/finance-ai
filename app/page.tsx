e8efc4a73499b9c39fd681bacb10e2c1e18062ef
Raul Santana <ti.raul77@gmail.com>
2024-11-07 21:40:43 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="text-red-500"> HELLO WORLD</h1>
-    </div>
+    <>
+      <div className="flex w-screen items-center justify-center">
+        <h1 className="text-red-500"> HELLO WORLD</h1>
+      </div>
+
+      <Button>Enviar</Button>
+    </>
   );
 };
 
