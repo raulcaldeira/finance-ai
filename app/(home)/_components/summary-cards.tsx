0edef28fa70368a72ab80775050ca7928fac7369
Raul Santana <ti.raul77@gmail.com>
2024-11-15 12:44:55 -0300

    feat: add summary card

---
+++ app/(home)/_components/summary-cards.tsx
@@ -0,0 +1,83 @@
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+import {
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from "lucide-react";
+import SummaryCard from "./summary-card";
+import { db } from "@/app/_lib/prisma";
+
+interface SummaryCards {
+  month: string;
+}
+
+const SummaryCards = async ({ month }: SummaryCards) => {
+  const where = {
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lte: new Date(`2024-${month}-31`),
+    },
+  };
+
+  const depositsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "DEPOSIT" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+
+  const investmentsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "INVESTMENT" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+
+  const expensesTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "EXPENSE" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+  const balance = depositsTotal - investmentsTotal - expensesTotal;
+
+  return (
+    <div className="space-y-6">
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={balance}
+        size="large"
+      />
+
+      <div className="grid grid-cols-3 gap-6">
+        <SummaryCard
+          icon={<PiggyBankIcon size={16} />}
+          title="Investido"
+          amount={investmentsTotal}
+        />
+
+        <SummaryCard
+          icon={<TrendingUpIcon className="text-primary" size={16} />}
+          title="Receita"
+          amount={depositsTotal}
+        />
+
+        <SummaryCard
+          icon={<TrendingDownIcon className="text-red-500" size={16} />}
+          title="Despesas"
+          amount={expensesTotal}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SummaryCards;
