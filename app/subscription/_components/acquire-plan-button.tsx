0a83a2644e85bc6418ec18ccab627023cb1d0af8
Raul Santana <ti.raul77@gmail.com>
2024-11-27 23:08:58 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -3,8 +3,14 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-strpe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const AcquirePlanButton = () => {
+  const { user } = useUser();
+
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
+
   const handleAcquirePlanClick = async () => {
     const { sessionId } = await createStripeCheckout();
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
@@ -21,12 +27,29 @@ const AcquirePlanButton = () => {
     await stripe.redirectToCheckout({ sessionId });
   };
 
+  if (hasPremiumPlan) {
+    return (
+      <Button
+        onClick={handleAcquirePlanClick}
+        className="w-full rounded-full font-bold"
+        variant="link"
+        asChild
+      >
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+          target="_blank"
+        >
+          Gerenciar Plano
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
       onClick={handleAcquirePlanClick}
       className="w-full rounded-full font-bold"
     >
-      Adquirir plano
+      Adquirir Plano
     </Button>
   );
 };
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,10 +1,10 @@
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import AcquirePlanButton from "./_components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const Subscription = async () => {
   const { userId } = await auth();
@@ -12,6 +12,9 @@ const Subscription = async () => {
   if (!userId) {
     redirect("/login");
   }
+
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
 
   return (
     <>
@@ -21,7 +24,12 @@ const Subscription = async () => {
 
         <div className="flex gap-6">
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {!hasPremiumPlan && (
+                <Badge className="text absolute left-6 top-8 bg-primary/10 text-base text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Básico
               </h2>
@@ -45,7 +53,12 @@ const Subscription = async () => {
           </Card>
 
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="text absolute left-6 top-8 bg-primary/10 text-base text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
