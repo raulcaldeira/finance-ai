09ebb73dbb602ec097e02160511a1bb213cb18f2
Raul Santana <ti.raul77@gmail.com>
2024-12-03 21:19:42 -0300

    feat: ensure transactions can be deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Mulish } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "./_components/ui/sonner";
 
 const mulish = Mulish({
   subsets: ["latin-ext"],
@@ -19,11 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${mulish.className} dark antialiased`}>
         <ClerkProvider appearance={{ baseTheme: dark }}>
           <div className="flex h-full flex-col overflow-hidden">{children}</div>
         </ClerkProvider>
+        <Toaster />
       </body>
     </html>
   );
